9f3e41a037a76bb21756e70df60de25faf0e79c4
Jezabel Danon <jezabel.danon@gmail.com>
2025-09-08 15:15:34 -0300

    Config ceedling

---
+++ test/test_leds.c
@@ -0,0 +1,27 @@
+/*
+ * 1. Prender un led y verificar que no cambia el resto.
+ * 2. Prender un led y apagarlo.
+ * 3. Prender más de un led, apagar uno y verificar que el resto siguen sin cambio.
+ * 4. Prender un led y apagarlo en ambos extremos (led 1 y led 16).
+ * 5. Iniciar el driver y revisar que todos los leds estén apagados.
+ * 6. Prender todos los leds.
+ * 7. Prender y apagar todos los leds.
+ * 8. Prender algunos leds más de una vez y verificar que siguen prendidos.
+ * 9. Apagar algunos leds más de una vez y verificar que siguen apagados.
+ * 10. Prender algunos leds, después prender todos y comprobar que todos los leds quedan encendidos.
+ * 11. Prender todos, apagar algunos, luego apagar todos y comprobar que todos los leds quedan apagados.
+ * 12. Consultar el estado de un led encendido y comprobar que es correcto.
+ * 13. Consultar el estado de un led apagado y comprobar que es correcto.
+ * 14. Intentar manipular un led fuera de rango y comprobar que se genera un error.
+*/
+#include "unity.h"
+
+void setUp(void){ // si no las creamos nosotros se crean automaticamente por ceedling vacias
+
+}
+void tearDown(void){
+
+}
+void test_inicial(void){
+	TEST_FAIL_MESSAGE("Arrancamos...");
+}
